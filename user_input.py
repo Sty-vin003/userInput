da42bb0729a52f2ee6b44142311df4e47f5a5e1d
Sty-vin003 <149212003+Sty-vin003@users.noreply.github.com>
2024-05-25 00:18:59 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = str(input("Please enter your name"))
+age = int(input("Please enter your age"))
+location = str(input("Emter you location"))
+
+strAge = str(age)
+
+print("Your name is " + name +", you are "+ strAge +" years old and you are from  " + location)
